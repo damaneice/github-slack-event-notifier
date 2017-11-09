cd5fa1d31bbe0e833af12eb343462e260556a65d
Damon Maneice <damaneice@gmail.com>
2017-11-09 15:25:02 -0500

    Added issues edited, milestoned, closed and reopened to supported events

--- index.js
+++ index.js
@@ -16,4 +16,60 @@ module.exports = (robot) => {
     const issueNotifier = new IssueNotifier(new SlackClient(config))
     issueNotifier.notify({owner: repoContext.owner, name: repoContext.repo}, {...issueDescription, action: 'opened'})
   })
+
+  robot.on('issues.edited', async context => {
+    const config = await context.config('config.yml', defaultConfig)
+    const issue = context.issue()
+
+    const issueDescription = await context.github.issues.get({
+      number: issue.number,
+      repo: config.repo,
+      owner: config.owner
+    })
+    const repoContext = context.repo()
+    const issueNotifier = new IssueNotifier(new SlackClient(config))
+    issueNotifier.notify({owner: repoContext.owner, name: repoContext.repo}, {...issueDescription, action: 'edited'})
+  })
+
+  robot.on('issues.milestoned', async context => {
+    const config = await context.config('config.yml', defaultConfig)
+    const issue = context.issue()
+
+    const issueDescription = await context.github.issues.get({
+      number: issue.number,
+      repo: config.repo,
+      owner: config.owner
+    })
+    const repoContext = context.repo()
+    const issueNotifier = new IssueNotifier(new SlackClient(config))
+    issueNotifier.notify({owner: repoContext.owner, name: repoContext.repo}, {...issueDescription, action: 'milestoned'})
+  })
+
+  robot.on('issues.closed', async context => {
+    const config = await context.config('config.yml', defaultConfig)
+    const issue = context.issue()
+
+    const issueDescription = await context.github.issues.get({
+      number: issue.number,
+      repo: config.repo,
+      owner: config.owner
+    })
+    const repoContext = context.repo()
+    const issueNotifier = new IssueNotifier(new SlackClient(config))
+    issueNotifier.notify({owner: repoContext.owner, name: repoContext.repo}, {...issueDescription, action: 'closed'})
+  })
+
+  robot.on('issues.reopened', async context => {
+    const config = await context.config('config.yml', defaultConfig)
+    const issue = context.issue()
+
+    const issueDescription = await context.github.issues.get({
+      number: issue.number,
+      repo: config.repo,
+      owner: config.owner
+    })
+    const repoContext = context.repo()
+    const issueNotifier = new IssueNotifier(new SlackClient(config))
+    issueNotifier.notify({owner: repoContext.owner, name: repoContext.repo}, {...issueDescription, action: 'reopened'})
+  })
 }
--- test/issue-notifier-test.js
+++ test/issue-notifier-test.js
@@ -8,24 +8,54 @@ describe('issue-notifier', () => {
     slackClient = {send: expect.createSpy()}
     issueNotifier = new IssueNotifier(slackClient)
   })
+  describe('slack client is called with', () => {
+    let repo
+    let issue
 
-  it('should call slack client with message', async () => {
-    const repo = {owner: 'owner', name: 'name'}
-    const issue = {
-      data: {
-        title: 'test',
-        html_url: 'http://www.example.com',
-        number: 2,
-        user: {
-          login: 'username'
+    beforeEach(() => {
+      repo = {owner: 'owner', name: 'name'}
+      issue = {
+        data: {
+          title: 'test',
+          html_url: 'http://www.example.com',
+          number: 2,
+          user: {
+            login: 'username'
+          }
         }
-      },
-      action: 'opened'
-    }
-    issueNotifier.notify(repo, issue)
-    const callArguments = slackClient.send.getLastCall().arguments
-    expect(slackClient.send).toHaveBeenCalled()
-    expect(callArguments[0]).toEqual('owner/name \n<http://www.example.com|Issue test #2> has been opened')
-    expect(callArguments[1]).toEqual('username')
+      }
+      slackClient = {send: expect.createSpy()}
+      issueNotifier = new IssueNotifier(slackClient)
+    })
+
+    it('opened message', async () => {
+      issue.action = 'opened'
+
+      issueNotifier.notify(repo, issue)
+      const callArguments = slackClient.send.getLastCall().arguments
+      expect(slackClient.send).toHaveBeenCalled()
+      expect(callArguments[0]).toEqual('owner/name \n<http://www.example.com|Issue test #2> has been opened')
+      expect(callArguments[1]).toEqual('username')
+    })
+
+    it('edited message', async () => {
+      issue.action = 'edited'
+
+      issueNotifier.notify(repo, issue)
+      const callArguments = slackClient.send.getLastCall().arguments
+      expect(slackClient.send).toHaveBeenCalled()
+      expect(callArguments[0]).toEqual('owner/name \n<http://www.example.com|Issue test #2> has been edited')
+      expect(callArguments[1]).toEqual('username')
+    })
+
+    it('reopened message', async () => {
+      issue.action = 'reopened'
+
+      issueNotifier.notify(repo, issue)
+      const callArguments = slackClient.send.getLastCall().arguments
+      expect(slackClient.send).toHaveBeenCalled()
+      expect(callArguments[0]).toEqual('owner/name \n<http://www.example.com|Issue test #2> has been reopened')
+      expect(callArguments[1]).toEqual('username')
+    })
   })
 })
